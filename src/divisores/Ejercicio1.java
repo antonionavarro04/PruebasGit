1f124028dbf7c8b207d5b00eeedf8b8702f2706b
Antonio Navarro <antonio.navarro@iesnervion.es>
2023-01-26 10:08:15 +0100

    Pruebas para el Examen de Bases de Datos

---
+++ src/divisores/Ejercicio1.java
@@ -0,0 +1,43 @@
+package divisores;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static final String MENSAJE = "Número de divisores: ";
+	private static Scanner lectura = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int numeroUsuario;
+		long numDivisores;
+		numeroUsuario = lectura.nextInt();
+		numDivisores = primeDivisors(numeroUsuario);
+		
+		System.out.println(MENSAJE + numDivisores);
+		lectura.close();
+	}
+
+	private static long primeDivisors(long number) {
+		int cont = 0;
+		
+		for(int i=2; i<=number; i++) {
+			if(number%i==0 && isPrime(i)) {
+				cont++;
+			}
+		}
+		
+		return cont;
+	}
+	
+	public static boolean isPrime(long number) {
+		boolean esPrimo = true;
+		for(int i=2; i<number; i++) {
+			long modulo = number % i;
+			if(modulo == 0) {
+				esPrimo = false;
+				break;
+			}
+		}
+		return esPrimo;
+	}
+}
